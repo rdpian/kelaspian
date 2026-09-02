375c772d0a04fbfe8b6ade8267f345cbe9a4d33f
unknown <putera.arya@gmail.com>
2026-09-02 17:39:10 +0700

    fix: add astro vue integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,35 +1,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import vue from '@astrojs/vue';
+
 export default defineConfig({
   // Ganti 'username' dengan nama akun GitHub milikmu
   site: 'https://rdpian.github.io',
   base: '/kelaspian', 
 
-  integrations: [
-    starlight({
-      title: 'Kelas Pian',
-      social: [
-        {
-          icon: 'github',
-          label: 'GitHub',
-          href: 'https://github.com/rdpian/kelaspian',
-        },
-      ],
-      sidebar: [
-        {
-          label: 'Kelas Moodle & Moodiy',
-          items: [
-            { autogenerate: { directory: 'moodle-moodiy' } }
-          ],
-        },
-        {
-          label: 'Kelas Pemrograman Micro:bit',
-          items: [
-            { autogenerate: { directory: 'microbit' } }
-          ],
-        },
-      ],
-    }),
-  ],
+  integrations: [starlight({
+    title: 'Kelas Pian',
+    social: [
+      {
+        icon: 'github',
+        label: 'GitHub',
+        href: 'https://github.com/rdpian/kelaspian',
+      },
+    ],
+    sidebar: [
+      {
+        label: 'Kelas Moodle & Moodiy',
+        items: [
+          { autogenerate: { directory: 'moodle-moodiy' } }
+        ],
+      },
+      {
+        label: 'Kelas Pemrograman Micro:bit',
+        items: [
+          { autogenerate: { directory: 'microbit' } }
+        ],
+      },
+    ],
+  }), vue()],
 });
